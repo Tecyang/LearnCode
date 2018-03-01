e2d15c214930dfa6400c5ea5075b75d11d335a45
tecyang <me@tecyang>
2018-03-01 22:23:45 +0800

    init project

---
+++ Code/LearnString.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tecyang on 18-3-1.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
+
---
+++ Code/cmake-build-debug/CMakeFiles/LearnString.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnString.dir/LearnString.cpp.o"
+  "LearnString.pdb"
+  "LearnString"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
